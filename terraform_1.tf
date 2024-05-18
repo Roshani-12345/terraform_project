5edfd36b7fdd2b962f95cef3a9d771db2ec83ba5
Roshani-12345 <Bagks2000@gmail.com>
2024-05-19 00:53:42 +0530

    first commit1

---
+++ terraform_1.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "ap-south-1"
+  #access_key = " "
+  #secret_key = " " 
+}
+
+resource "aws_instance" "intro" {
+  ami                    = "ami-0f58b397bc5c1f2e8"
+  instance_type          = "t2.micro"
+  availability_zone      = "ap-south-1a"
+  key_name               = "dove_key"
+  vpc_security_group_ids = ["sg-0491958282729300d"]
+  tags = {
+    Name = "Dove-Instance"
+  }
+}
+
+
+
